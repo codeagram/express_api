15492513fffd155984967638a0bad264bc89abc0
codeagram <arun.akumar-ak@protonmail.com>
2023-01-27 15:13:00 +0530

    removed dependance: agenda

--- addData.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO public."Branch" (branchName, branchCode) VALUES ('Gobi', '1GOB'), ('Mulanur', '2MLR'), ('Pollachi', '3POL'), ('Coimbatore', '4CBE'), ('Tirupur', '5TPR'), ('Erode', '6ERD'), ('Dindigul', '7DGL'), ('Karur', '8KRR');
